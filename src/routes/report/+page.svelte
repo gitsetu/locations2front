ccca8b1b332530112fb670bc1d1eb13eec8c5d20
gitsetu <pedroroyo@hotmail.com>
2025-05-26 21:14:53 +0100

    lab 19b cards

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+    import { subTitle } from "$lib/runes.svelte";
+    import Card from "$lib/ui/Card.svelte";
     import DonationList from "$lib/ui/DonationList.svelte";
+
+    subTitle.text = "Donations to date";
 </script>
 
-<DonationList />
+<Card title="Donations">
+  <DonationList />
+</Card>
