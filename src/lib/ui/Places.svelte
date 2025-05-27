33285b886c4bdaeefd30f81d0add5722556689b3
gitsetu <pedroroyo@hotmail.com>
2025-05-27 15:40:43 +0100

    added report table

--- src/lib/ui/Places.svelte
+++ src/lib/ui/Places.svelte
@@ -1,43 +1,21 @@
 <script lang="ts">
-    export let places = [
-        {
-            title: 40,
-            category: "paypal",
-            donor: "bart@simpson.com",
-            collection: { lastName: "Simpson", firstName: "Lisa" },
-            latitude: "52.161290",
-            longitude: "-7.51540"
-        },
-        {
-            title: 90,
-            category: "direct",
-            donor: "marge@simpson.com",
-            collection: { lastName: "Simpson", firstName: "maggie" },
-            latitude: "52.261290",
-            longitude: "-7.231540"
-        },
-        {
-            title: 430,
-            category: "paypal",
-            donor: "homer@simpson.com",
-            collection: { lastName: "Flanders", firstName: "Ned" },
-            latitude: "52.361290",
-            longitude: "-7.241540"
-        }
-    ];
+    let { places } = $props();
 </script>
 
 <table class="table is-fullwidth">
   <thead>
   <tr>
-    <th>Place</th>
+    <th>Place Name</th>
     <th>Category</th>
+    <th>Collection</th>
     <th>Latitude</th>
     <th>Longitude</th>
+    <th>User</th>
   </tr>
   </thead>
   <tbody>
   {#each places as place}
+
     <tr>
       <td>
         {place.title}
@@ -46,10 +24,10 @@
         {place.category}
       </td>
       <td>
-        {place.latitude}
+        {place.collection.title}
       </td>
       <td>
-        {place.longitude}
+        {place.user.firstName}, {place.user.lastName}
       </td>
     </tr>
   {/each}
--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
-    import { subTitle } from "$lib/runes.svelte";
+    // import { subTitle } from "$lib/runes.svelte";
     import Card from "$lib/ui/Card.svelte";
     import Places from "$lib/ui/Places.svelte";
+    import { loggedInUser, subTitle } from "$lib/runes.svelte";
+    import { appService } from "$lib/services/app-service";
+    import { onMount } from "svelte";
+    import type { Place } from "$lib/types/app-types";
 
     subTitle.text = "Places added";
+
+    let places: Place[] = [];
+    onMount(async () => {
+        places = await appService.getPlaces(loggedInUser.token);
+    });
 </script>
 
 <Card title="Places">
-  <Places />
+  <Places {places}/>
 </Card>
